4e0ca8c676a59309c76bd73f290a27301b85c069
Steve King <kingshizzle@gmail.com>
2019-01-13 20:19:38 -0800

    Output is lame, but seems to work.

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use ndarray::Array2;
+use ndarray::Array3;
+use rand::{thread_rng, Rng};
+
+fn is_valid_case(draws: usize, have: usize, need: usize) -> bool {
+    return !((draws == 0) || (need == 0) || (need > draws) || ((need + have) > 6));
+}
+
+fn dump(ref pa : ndarray::Array3::<f32>) {
+    for ((draws, have, need), slice) in pa.indexed_iter() {
+        if is_valid_case(draws, have, need) {
+            println!("{} {} {} = {}", draws, have, need, pa[[draws, have, need]] );
+        }
+    }
+}
+
+// The have's fill in the lower indexes.
+// The need's are the upper indexes.
+fn do_draws(draws: usize, have :usize, need: usize) -> bool {
+    let mut rng = rand::thread_rng();
+    let mut hits : [usize; 6] = [0; 6];
+
+    for _ in 0..draws {
+        let draw = rng.gen_range(0,6);
+        hits[draw] += 1;
+    }
+
+    // For success, at least 'need' values above 'have' must be non-zero
+    let mut good_count = 0;
+    for i in have..6 {
+        if hits[i] > 0 {
+            good_count += 1;
+        }
+    }
+    return good_count >= need;
+}
+
+// The have's fill in the lower indexes.
+// The need's are the upper indexes.
+fn calc_probability(draws: usize, have :usize, need: usize) -> f32 {
+    const SAMPLES : usize = 1000000;
+    let mut wins : usize = 0;
+    for _ in 0..SAMPLES {
+        if do_draws(draws, have, need) {
+            wins += 1;
+        }
+    }
+    let fwins = wins as f32;
+    let fsamples = SAMPLES as f32;
+    return fwins / fsamples;
+}
+
+fn main() {
+    // Declare the probability array
+    // Outer index (slowest changing) is number of draws
+    // middle index is the number already owned
+    // inner index (fastest changing) is the number needed
+    let mut pa = ndarray::Array3::<f32>::zeros((6,6,6));
+    for ((draws, have, need), elem) in pa.indexed_iter_mut() {
+        if !is_valid_case(draws, have, need) {
+            continue;
+        }
+        *elem = calc_probability(draws, have, need) * 100.0;
+        println!("{} {} {} = {}", draws, have, need, elem );
+    }
+
+    dump(pa);
+}
