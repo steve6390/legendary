3f6743317ec28cb720584a70881779e16aa1afe6
Steve King <kingshizzle@gmail.com>
2019-01-13 22:08:26 -0800

    Improved output readability.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use ndarray::Array2;
-use ndarray::Array3;
-use rand::{thread_rng, Rng};
+use ndarray::{Array2, Array3, Axis, aview2};
+use rand::Rng;
 
+const MAX_DRAWS: usize = 6;
 fn is_valid_case(draws: usize, have: usize, need: usize) -> bool {
     return !((draws == 0) || (need == 0) || (need > draws) || ((need + have) > 6));
 }
 
 fn dump(ref pa : ndarray::Array3::<f32>) {
-    for ((draws, have, need), slice) in pa.indexed_iter() {
-        if is_valid_case(draws, have, need) {
-            println!("{} {} {} = {}", draws, have, need, pa[[draws, have, need]] );
+    let mut draws = 0;
+    for perdraw in pa.axis_iter(Axis(0)) {
+        println!("{} Draws ---------------", draws);
+        println!("{:<5} {:<5} {:<12}", "Have", "Need", "Probability");
+        for ((have, need), elem) in perdraw.indexed_iter() {
+            if is_valid_case(draws, have, need) {
+                println!("{:<5} {:<5} {:<12.1}", have, need, elem );
+            }
         }
+        println!("------------------------\n");
+        draws += 1;
     }
 }
 
@@ -38,7 +45,7 @@ fn do_draws(draws: usize, have :usize, need: usize) -> bool {
 // The have's fill in the lower indexes.
 // The need's are the upper indexes.
 fn calc_probability(draws: usize, have :usize, need: usize) -> f32 {
-    const SAMPLES : usize = 1000000;
+    const SAMPLES : usize = 100000;
     let mut wins : usize = 0;
     for _ in 0..SAMPLES {
         if do_draws(draws, have, need) {
@@ -55,7 +62,7 @@ fn main() {
     // Outer index (slowest changing) is number of draws
     // middle index is the number already owned
     // inner index (fastest changing) is the number needed
-    let mut pa = ndarray::Array3::<f32>::zeros((6,6,6));
+    let mut pa = ndarray::Array3::<f32>::zeros((MAX_DRAWS+1,6,6));
     for ((draws, have, need), elem) in pa.indexed_iter_mut() {
         if !is_valid_case(draws, have, need) {
             continue;
